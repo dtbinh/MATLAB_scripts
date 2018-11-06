e85bf442379c51e65b5e047bfbc15433ab6c19ec
Dan Sullivan <dtscbs@verizon.net>
2018-11-06 14:11:42 -0500

    Complete reading the config file

---
+++ readConfig.m
@@ -0,0 +1,19 @@
+function ports = readConfig()
+% Read the configuration file - one COM port per line
+
+ports = {'', '', '' };
+fileID = fopen('com.txt');
+
+tline = fgetl(fileID);
+index = 1;
+i = 1;
+while ischar(tline)
+    cell_array = strsplit(tline);
+    ports{index} = cell_array{i};
+    %fprintf("zzz found port %s\n", cell_array{i});
+    index = index + 1;
+    tline = fgetl(fileID);
+end
+
+fclose(fileID);
+end
