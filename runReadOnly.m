7a3ed9c501a5a25246945657da294eb376139094
Dan <dtscbs@verizon.net>
2018-11-08 11:41:39 -0500

    Remove printDevice function to a seperate file

--- runReadOnly.m
+++ runReadOnly.m
@@ -51,7 +51,8 @@ varsToStream = [ 		...
 	FX_RIGID_MOT_VOLT		...
 ];
 
-outVars = [ 99, 99, 99, 99, 99, 99, 99, 99, 99, 99 ];
+    outVars = [ 99, 99, 99, 99, 99, 99, 99, 99, 99, 99 ];
+    
     % Select the variables to stream
     [retCode, outVars ] = calllib(libHandle, 'fxSetStreamVariables', deviceId,  varsToStream, 10 );
     
@@ -69,27 +70,4 @@ outVars = [ 99, 99, 99, 99, 99, 99, 99, 99, 99, 99 ];
         end
     end
     calllib(libHandle, 'fxStopStreaming', deviceId);
-end
-
-function printDevice( libHandle, devId, vars, labels, n)
-% Read the variables from the device and print them
-    success = [ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ];
-    retData = [ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ];
-
-    % Read the values from the device
-    [ ptr, retData, success] = calllib(libHandle, 'fxReadDevice', devId, vars, success, n);
-
-    % Jump through some hoops to get access to the returned data
-    ptrindex = libpointer('int32Ptr', zeros(1:10, 'int32'));
-    ptrindex = ptr;
-    setdatatype(ptrindex, 'int32Ptr', 1, 10);
-    
-    % Print the data or failure indication
-    for i = 1:length( vars )
-        if( success(i) )
-            fprintf("\t%14s\t%d\n", labels{i}, ptrindex.value(i) );
-        else
-            fprintf("\t%14s\t------\n", labels{i} );
-        end
-    end
 end
